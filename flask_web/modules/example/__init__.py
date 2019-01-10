5c879b4b949d56210dfd6500d6faccc4ebcacc47
Zheng <zheng@111.com>
2019-01-10 16:16:55 +0800

    增加flask_restful组建 类视图

--- flask_web/modules/example/__init__.py
+++ flask_web/modules/example/__init__.py
@@ -1,7 +1,13 @@
 from flask import Blueprint
+from flask_restful import Api
 
 # 创建蓝图对象 # url_prefix="/example" 前缀
 example_blue = Blueprint('example_blue', __name__)
 
+docs = Api(example_blue)
+
 # 把使用蓝图对象的文件导入到创建蓝图对象的下面
 from . import views
+
+# 绑定路由
+docs.add_resource(views.Index, "/index/<int:id>")
--- flask_web/modules/example/views.py
+++ flask_web/modules/example/views.py
@@ -4,29 +4,15 @@ from flask import session, redirect, render_template, current_app, jsonify, requ
 from flask_web import db
 # 导入自定义的状态码
 from flask_web.utils.response_code import RET
+from flask_restful import Resource
+
 from . import example_blue
 
 
-# json渲染
-@example_blue.route('/')
-def index():
-    return "Hello flask_web"
+class Index(Resource):
 
+    def get(self, id):
+        return {"GET欢迎用户": id}
 
-# 模板渲染
-@example_blue.route('/templates')
-def hello():
-    data = {"data": "example"}
-    return render_template('example/index.html', data=data)
-
-
-# 路由过滤器+json渲染
-@example_blue.route('/<int:id>')
-def user(id):
-    return jsonify({"欢迎用户": id})
-
-
-# 重定向
-@example_blue.route('/test')
-def index_url_for():
-    return redirect(url_for("example_blue.index"))
+    def post(self, id):
+        return "这是post请求"
